dd0ec501064aff958ad4016f65d6569fa2205d42
Martin Novák <martin.novak@gmx.com>
2023-04-28 22:39:13 +0200

    improved pawn moves generating

--- movegen.h
+++ movegen.h
@@ -15,11 +15,10 @@ void generate_pawn_moves(board & b, movelist & ml, uint64_t checkmask, uint64_t 
     constexpr Direction up_2 = (our_color == WHITE ? NORTH_2 : SOUTH_2);
     constexpr Direction antidiagonal_capture = (our_color == WHITE ? NORTH_WEST : SOUTH_EAST);
     constexpr Direction diagonal_capture = (our_color == WHITE ? NORTH_EAST : SOUTH_WEST);
-
     constexpr Direction antidiagonal_enpassant = (our_color == BLACK ? NORTH_WEST : SOUTH_EAST);
     constexpr Direction diagonal_enpassant = (our_color == BLACK ? NORTH_EAST : SOUTH_WEST);
 
-    const uint64_t pawns = b.get_pawn_bitboard();
+    const uint64_t pawns = b.get_pawn_bitboard() & move_h; // horizontally pinned pawns cannot move
 
     const uint64_t pawns_penultimate = pawns & penultimate_rank;
     const uint64_t pawns_not_penultimate = pawns & not_penultimate_rank;
@@ -36,12 +35,12 @@ void generate_pawn_moves(board & b, movelist & ml, uint64_t checkmask, uint64_t 
 
     while(bitboard_push_1) {
         int pawn_square = pop_lsb(bitboard_push_1);
-        ml.add(move(pawn_square - up, pawn_square, our_color, PAWN));
+        ml.add(move_t(pawn_square - up, pawn_square, our_color, PAWN));
     }
 
     while(bitboard_push_2) {
         int pawn_square = pop_lsb(bitboard_push_2);
-        ml.add(move(pawn_square - up_2, pawn_square, our_color, PAWN, DOUBLE_PAWN_PUSH));
+        ml.add(move_t(pawn_square - up_2, pawn_square, our_color, PAWN, DOUBLE_PAWN_PUSH));
     }
 
     // PAWNS PROMOTE
@@ -53,28 +52,28 @@ void generate_pawn_moves(board & b, movelist & ml, uint64_t checkmask, uint64_t 
 
         while (bitboard_promote_push) {
             int pawn_square = pop_lsb(bitboard_promote_push);
-            ml.add(move(pawn_square - up, pawn_square, our_color, PAWN, QUEEN_PROMOTION));
-            ml.add(move(pawn_square - up, pawn_square, our_color, PAWN, ROOK_PROMOTION));
-            ml.add(move(pawn_square - up, pawn_square, our_color, PAWN, BISHOP_PROMOTION));
-            ml.add(move(pawn_square - up, pawn_square, our_color, PAWN, KNIGHT_PROMOTION));
+            ml.add(move_t(pawn_square - up, pawn_square, our_color, PAWN, QUEEN_PROMOTION));
+            ml.add(move_t(pawn_square - up, pawn_square, our_color, PAWN, ROOK_PROMOTION));
+            ml.add(move_t(pawn_square - up, pawn_square, our_color, PAWN, BISHOP_PROMOTION));
+            ml.add(move_t(pawn_square - up, pawn_square, our_color, PAWN, KNIGHT_PROMOTION));
         }
 
         while (bitboard_promote_antidiagonal) {
             int pawn_square = pop_lsb(bitboard_promote_antidiagonal);
             PieceType attacked_piece = b.get_piece(pawn_square);
-            ml.add(move(pawn_square - antidiagonal_capture, pawn_square, our_color, PAWN, QUEEN_PROMOTION_CAPTURE, attacked_piece));
-            ml.add(move(pawn_square - antidiagonal_capture, pawn_square, our_color, PAWN, ROOK_PROMOTION_CAPTURE, attacked_piece));
-            ml.add(move(pawn_square - antidiagonal_capture, pawn_square, our_color, PAWN, BISHOP_PROMOTION_CAPTURE, attacked_piece));
-            ml.add(move(pawn_square - antidiagonal_capture, pawn_square, our_color, PAWN, KNIGHT_PROMOTION_CAPTURE, attacked_piece));
+            ml.add(move_t(pawn_square - antidiagonal_capture, pawn_square, our_color, PAWN, QUEEN_PROMOTION_CAPTURE, attacked_piece));
+            ml.add(move_t(pawn_square - antidiagonal_capture, pawn_square, our_color, PAWN, ROOK_PROMOTION_CAPTURE, attacked_piece));
+            ml.add(move_t(pawn_square - antidiagonal_capture, pawn_square, our_color, PAWN, BISHOP_PROMOTION_CAPTURE, attacked_piece));
+            ml.add(move_t(pawn_square - antidiagonal_capture, pawn_square, our_color, PAWN, KNIGHT_PROMOTION_CAPTURE, attacked_piece));
         }
 
         while (bitboard_promote_diagonal) {
             int pawn_square = pop_lsb(bitboard_promote_diagonal);
             PieceType attacked_piece = b.get_piece(pawn_square);
-            ml.add(move(pawn_square - diagonal_capture, pawn_square, our_color, PAWN, QUEEN_PROMOTION_CAPTURE, attacked_piece));
-            ml.add(move(pawn_square - diagonal_capture, pawn_square, our_color, PAWN, ROOK_PROMOTION_CAPTURE, attacked_piece));
-            ml.add(move(pawn_square - diagonal_capture, pawn_square, our_color, PAWN, BISHOP_PROMOTION_CAPTURE, attacked_piece));
-            ml.add(move(pawn_square - diagonal_capture, pawn_square, our_color, PAWN, KNIGHT_PROMOTION_CAPTURE, attacked_piece));
+            ml.add(move_t(pawn_square - diagonal_capture, pawn_square, our_color, PAWN, QUEEN_PROMOTION_CAPTURE, attacked_piece));
+            ml.add(move_t(pawn_square - diagonal_capture, pawn_square, our_color, PAWN, ROOK_PROMOTION_CAPTURE, attacked_piece));
+            ml.add(move_t(pawn_square - diagonal_capture, pawn_square, our_color, PAWN, BISHOP_PROMOTION_CAPTURE, attacked_piece));
+            ml.add(move_t(pawn_square - diagonal_capture, pawn_square, our_color, PAWN, KNIGHT_PROMOTION_CAPTURE, attacked_piece));
         }
     }
 
@@ -85,12 +84,12 @@ void generate_pawn_moves(board & b, movelist & ml, uint64_t checkmask, uint64_t 
 
     while(bitboard_left_capture) {
         int pawn_square = pop_lsb(bitboard_left_capture);
-        ml.add(move(pawn_square - antidiagonal_capture, pawn_square, our_color, PAWN, CAPTURE, b.get_piece(pawn_square)));
+        ml.add(move_t(pawn_square - antidiagonal_capture, pawn_square, our_color, PAWN, CAPTURE, b.get_piece(pawn_square)));
     }
 
     while(bitboard_right_capture) {
         int pawn_square = pop_lsb(bitboard_right_capture);
-        ml.add(move(pawn_square - diagonal_capture, pawn_square, our_color, PAWN, CAPTURE, b.get_piece(pawn_square)));
+        ml.add(move_t(pawn_square - diagonal_capture, pawn_square, our_color, PAWN, CAPTURE, b.get_piece(pawn_square)));
     }
 
     // en passant
@@ -101,11 +100,11 @@ void generate_pawn_moves(board & b, movelist & ml, uint64_t checkmask, uint64_t 
         uint64_t enpassant_bitboard = (1ull << enpassant_square);
         uint64_t enpassant_antidiagonal = shift<antidiagonal_capture>(pawns_not_penultimate & move_a) & enpassant_bitboard;
         if(enpassant_antidiagonal && b.check_legality_of_enpassant<their_color>(enpassant_square - antidiagonal_capture, enpassant_square - up)) {
-            ml.add(move(enpassant_square - antidiagonal_capture, enpassant_square, our_color, PAWN, EN_PASSANT, PAWN));
+            ml.add(move_t(enpassant_square - antidiagonal_capture, enpassant_square, our_color, PAWN, EN_PASSANT, PAWN));
         }
         uint64_t enpassant_diagonal = shift<diagonal_capture>(pawns_not_penultimate & move_d) & enpassant_bitboard;
         if(enpassant_diagonal && b.check_legality_of_enpassant<their_color>(enpassant_square - diagonal_capture, enpassant_square - up)) {
-            ml.add(move(enpassant_square - diagonal_capture, enpassant_square, our_color, PAWN, EN_PASSANT, PAWN));
+            ml.add(move_t(enpassant_square - diagonal_capture, enpassant_square, our_color, PAWN, EN_PASSANT, PAWN));
         }
 
 
@@ -115,7 +114,7 @@ void generate_pawn_moves(board & b, movelist & ml, uint64_t checkmask, uint64_t 
         while (b1) {
             int square_from = pop_lsb(b1);
             if (b.check_legality_of_enpassant<their_color>(square_from, enpassant_square - up)) {
-                ml.add(move(square_from, enpassant_square, our_color, PAWN, EN_PASSANT, PAWN));
+                ml.add(move_t(square_from, enpassant_square, our_color, PAWN, EN_PASSANT, PAWN));
             }
         }
         */
@@ -142,12 +141,12 @@ void generate_slider_moves(board & b, movelist & ml, uint64_t piece_bitboard, ui
 
         while(captures) {
             int target_square = pop_lsb(captures);
-            ml.add(move(square, target_square, our_color, pt, CAPTURE, b.get_piece(target_square)));
+            ml.add(move_t(square, target_square, our_color, pt, CAPTURE, b.get_piece(target_square)));
         }
 
         while(quiets) {
             int target_square = pop_lsb(quiets);
-            ml.add(move(square, target_square, our_color, pt));
+            ml.add(move_t(square, target_square, our_color, pt));
         }
     }
 }
@@ -155,19 +154,19 @@ void generate_slider_moves(board & b, movelist & ml, uint64_t piece_bitboard, ui
 template <Color our_color>
 void generate_knight_moves(board & b, movelist & ml, uint64_t target, uint64_t pin, uint64_t enemy, uint64_t empty, uint64_t occupancy)
 {
-    uint64_t piece_bitboard = b.get_knight_bitboard() & pin; // pinned knight cannot move
+    uint64_t piece_bitboard = b.get_knight_bitboard() & pin; // pinned knight cannot move_t
     while(piece_bitboard) {
         int square = pop_lsb(piece_bitboard);
         uint64_t attack_bitboard = attacks<KNIGHT>(square, occupancy) & target;
         uint64_t captures = attack_bitboard & enemy;
         while(captures) {
             int target_square = pop_lsb(captures);
-            ml.add(move(square, target_square, our_color, KNIGHT, CAPTURE, b.get_piece(target_square)));
+            ml.add(move_t(square, target_square, our_color, KNIGHT, CAPTURE, b.get_piece(target_square)));
         }
         uint64_t quiets = attack_bitboard & empty;
         while(quiets) {
             int target_square = pop_lsb(quiets);
-            ml.add(move(square, target_square, our_color, KNIGHT));
+            ml.add(move_t(square, target_square, our_color, KNIGHT));
         }
     }
 }
@@ -180,13 +179,13 @@ void generate_king_moves(board &b, movelist & ml, int king_square, uint64_t safe
     uint64_t king_quiets = king_moves & empty;
     while(king_quiets) {
         int target_square = pop_lsb(king_quiets);
-        ml.add(move(king_square, target_square, our_color, KING));
+        ml.add(move_t(king_square, target_square, our_color, KING));
     }
 
     uint64_t king_captures = king_moves & enemy;
     while (king_captures) {
         int target_square = pop_lsb(king_captures);
-        ml.add(move(king_square, target_square, our_color, KING, CAPTURE, b.get_piece(target_square)));
+        ml.add(move_t(king_square, target_square, our_color, KING, CAPTURE, b.get_piece(target_square)));
     }
 }
 
@@ -206,14 +205,14 @@ void generate_castle_moves(movelist & ml, int castling_right, uint64_t safe_squa
     && (safe_squares & kingside_castle_efg_mask) == kingside_castle_efg_mask
     && (empty & kingside_castle_fg_mask) == kingside_castle_fg_mask)
     {
-        ml.add(move(king_e_square, king_g_square, our_color, KING, KING_CASTLE));
+        ml.add(move_t(king_e_square, king_g_square, our_color, KING, KING_CASTLE));
     }
 
     if(castling_right & queenside_castle_right
        && (safe_squares & queenside_castle_cde_mask) == queenside_castle_cde_mask
        && (empty & queenside_castle_bcd_mask) == queenside_castle_bcd_mask)
     {
-        ml.add(move(king_e_square, king_c_square, our_color, KING, QUEEN_CASTLE));
+        ml.add(move_t(king_e_square, king_c_square, our_color, KING, QUEEN_CASTLE));
     }
 }
 
@@ -222,8 +221,10 @@ void generate_all_moves(board & b, movelist & ml) {
     constexpr Color enemy_color = our_color == WHITE ? BLACK : WHITE;
 
     int king_square = b.get_king_square();
-
-    auto [friendly_pieces, enemy_pieces, occupancy, empty, checkers] = b.get_bitboards<our_color, enemy_color>();
+    uint64_t enemy_pieces = b.get_enemy_bitboard<enemy_color>();
+    uint64_t occupancy = b.get_occupancy();
+    uint64_t empty = ~occupancy;
+    uint64_t checkers = b.get_checkers<enemy_color>();
 
     uint64_t safe_squares = b.get_safe_squares<enemy_color>(king_square);
 
@@ -234,10 +235,12 @@ void generate_all_moves(board & b, movelist & ml) {
     if(checkmask == 0)
         return;
 
-    auto [pin_hv, pin_ad, move_h, move_v, move_a, move_d] = b.get_pinners<our_color, enemy_color>(king_square);
-    auto [rook_bitboard, bishop_bitboard, queen_bitboard] = b.get_slider_bitboards();
-    rook_bitboard &= ~pin_ad; // rooks pinned diagonally cannot move
-    bishop_bitboard &= ~pin_hv; // bishops pinned horizontally and vertically cannot move
+    auto [pin_h, pin_v, pin_a, pin_d] = b.get_pinners<our_color, enemy_color>(king_square);
+    auto [rook_bitboard, bishop_bitboard, queen_bitboard] = b.get_slider_bitboards<our_color>();
+    uint64_t pin_hv = pin_h | pin_v;
+    uint64_t pin_ad = pin_a | pin_d;
+    rook_bitboard &= ~pin_ad; // rooks pinned diagonally cannot move_t
+    bishop_bitboard &= ~pin_hv; // bishops pinned horizontally and vertically cannot move_t
     uint64_t non_pinned_pieces = ~(pin_ad | pin_hv);
 
     uint64_t hv_checkmask = checkmask & pin_hv;
@@ -259,7 +262,7 @@ void generate_all_moves(board & b, movelist & ml) {
     generate_slider_moves<our_color, QUEEN, false, true>(b, ml, pin_ad & queen_bitboard, ad_checkmask, enemy_pieces, empty, occupancy);
 
     generate_knight_moves<our_color>(b, ml,checkmask, non_pinned_pieces, enemy_pieces, empty, occupancy);
-    generate_pawn_moves<our_color>(b, ml, checkmask, move_h, move_v, move_a, move_d, enemy_pieces, empty);
+    generate_pawn_moves<our_color>(b, ml, checkmask, ~pin_h, ~(pin_a | pin_d), ~(pin_d | pin_v), ~(pin_a | pin_v), enemy_pieces, empty);
     generate_castle_moves<our_color>(ml, b.get_castle_rights(),  safe_squares, empty);
 }
 
