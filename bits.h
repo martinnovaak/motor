c0e912b964fc5859534edc750c89e1bae684c341
Martin Novák <martin.novak@gmx.com>
2023-04-30 21:36:56 +0200

    MSVC and GCC upgrade

--- bits.h
+++ bits.h
@@ -19,22 +19,22 @@ static void print_bitboard(uint64_t bitboard)
     std::cout << "\n\n";
 }
 
-constexpr static bool get_bit(uint64_t bitboard, int square) {
+static bool get_bit(uint64_t bitboard, int square) {
     uint64_t mask = 1ull << square; // create a mask with a 1 at the i-th bit
     return bitboard & mask; // return true if the i-th bit is 1, false otherwise
 }
 
-constexpr static void set_bit(uint64_t& bb, int sq) {
+static void set_bit(uint64_t& bb, int sq) {
     bb |= (1ULL << sq);
 }
 
-constexpr static void pop_bit(uint64_t& bb, int sq) {
+static void pop_bit(uint64_t& bb, int sq) {
     bb &= ~(1ULL << sq);
 }
 
 #if defined(__GNUC__) // GCC, Clang, ICC
 
-constexpr static int lsb(uint64_t bitboard)
+static int lsb(uint64_t bitboard)
 {
     return __builtin_ctzll(bitboard);
 }
@@ -47,24 +47,24 @@ static int msb(uint64_t bitboard)
 #elif defined(_MSC_VER) // MSVC
 #ifdef _WIN64 // MSVC, WIN64
 #include <intrin.h>
-inline Square lsb(U64 b)
+static int lsb(uint64_t b)
 {
     unsigned long idx;
     _BitScanForward64(&idx, b);
-    return (Square)idx;
+    return idx;
 }
-inline Square msb(U64 b)
+static int msb(uint64_t b)
 {
     unsigned long idx;
     _BitScanReverse64(&idx, b);
-    return (Square)idx;
+    return idx;
 }
 #endif
 #else
 #error "Compiler not supported."
 #endif
 
-constexpr static uint8_t popcount(uint64_t mask)
+static uint8_t popcount(uint64_t mask)
 {
 #if defined(_MSC_VER) || defined(__INTEL_COMPILER)
     return (uint8_t)_mm_popcnt_u64(mask);
@@ -78,7 +78,7 @@ constexpr static uint64_t pop_bits(uint64_t bitboard, int square1, int square2) 
     return bitboard & ~(1ULL << (square2));
 }
 
-constexpr static int pop_lsb(uint64_t & bitboard) {
+static int pop_lsb(uint64_t & bitboard) {
     const int index = lsb(bitboard);
     bitboard &= bitboard - 1;
     return index;
