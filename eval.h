b1f3ee16969dfa195824f6f2555fe3f0fadd0eba
Martin Novák <martin.novak@gmx.com>
2023-05-29 22:24:19 +0200

    Simple counting pieces evaluation

---
+++ eval.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "board.h"
+constexpr int piece_values[N_COLORS][N_PIECE_TYPES] = {
+        {100, 300, 300, 500, 950 , 20'000}, {-100, -300, -300, -500, -950, -20'000}
+};
+
+static int evaluate(board& b) {
+    int score = 0;
+
+    for (int color = 0; color <= 1; color++) {
+        for (int piece = 0; piece < 5; piece++) {
+            uint64_t bitboard = b.get_pieces(color, piece);
+
+            while (bitboard) {
+                pop_lsb(bitboard);
+                score += piece_values[color][piece] ;
+            }
+        }
+    }
+
+    return score;
+}
