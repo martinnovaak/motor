668728c8b77ef070e16455ac99076c9fab9faf31
Martin Novák <martin.novak@gmx.com>
2023-06-15 18:09:52 +0200

    Repair stopwatch when movestogo are not given

--- stopwatch_t.h
+++ stopwatch_t.h
@@ -11,13 +11,18 @@ public:
         start_time = std::chrono::steady_clock::now();
         stop = false;
         infinite_time = false;
+        max_nodes = INT_MAX / 2;
 
         if(movetime > 0) {
             time_limit = movetime;
         } else if(time == -1) {
             infinite_time = true;
         } else {
-            time_limit = increment + 975 * time / movestogo / 1000;
+            if(movestogo == 0) {
+                time_limit = increment + time / 50;
+            } else {
+                time_limit = increment + 975 * time / movestogo / 1000;
+            }
         }
     }
 
@@ -29,7 +34,7 @@ public:
         if (stop) {
             return true;
         } else if((nodes & 1024) && (!infinite_time)) {
-            stop = elapsed() >= time_limit;
+            stop = elapsed() >= time_limit || nodes >= max_nodes;
         }
         return stop;
     }
@@ -38,11 +43,25 @@ public:
         return static_cast<int>(std::chrono::duration_cast<std::chrono::duration<int, std::milli>>(std::chrono::steady_clock::now() - start_time).count());
     }
 
+    uint64_t NPS(uint64_t nodes){
+        uint64_t elapsed_time = elapsed();
+        if(elapsed_time > 0) {
+            return (nodes / elapsed_time) * 1000;
+        } else {
+            return 0;
+        }
+    }
+
+    void stop_timer() {
+        stop = true;
+    }
+
 private:
     std::chrono::time_point<std::chrono::steady_clock> start_time;
     bool stop;
     int time_limit;
     bool infinite_time;
+    int max_nodes;
 };
 
 #endif //MOTOR_TIME_H
