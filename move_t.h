6db3bd48d7d6c291d65381fbb00443b48b0a6d3f
Martin Novák <martin.novak@gmx.com>
2023-06-25 18:20:19 +0200

    Save score with flags into one uint32_t
    Score of dev vs old: 1209 - 1058 - 1486  [0.520] 3753
    ...      dev playing White: 635 - 524 - 716  [0.530] 1875
    ...      dev playing Black: 574 - 534 - 770  [0.511] 1878
    ...      White vs Black: 1169 - 1098 - 1486  [0.509] 3753
    Elo difference: 14.0 +/- 8.6, LOS: 99.9 %, DrawRatio: 39.6 %
    SPRT: llr 2.96 (100.5%), lbound -2.94, ubound 2.94 - H1 was accepted
    Finished match

--- move_t.h
+++ move_t.h
@@ -1,8 +1,6 @@
 #ifndef MOTOR_MOVE_T_H
 #define MOTOR_MOVE_T_H
 
-#include <iostream>
-
 enum MoveType : int {
     QUIET                    = 0,
     DOUBLE_PAWN_PUSH         = 1,
@@ -22,58 +20,71 @@ enum MoveType : int {
 
 struct move_t {
 public:
-    move_t() : m_move{} {}
+    [[nodiscard]] move_t() : m_move{} {}
 
-    move_t(int from, int to, Color color, int moving_piece, int type_information, int captured_piece) {
+    [[nodiscard]] move_t(int from, int to, Color color, int moving_piece, int type_information, int captured_piece) {
         m_move = from | to << 6 | color << 12 | moving_piece << 13 | type_information << 16 | captured_piece << 20;
     }
 
-    move_t(int from, int to, Color color, int moving_piece, int type_information) {
+    [[nodiscard]] move_t(int from, int to, Color color, int moving_piece, int type_information) {
         m_move = from | to << 6 | color << 12 | moving_piece << 13 | type_information << 16 | KING << 20;
     }
 
-    move_t(int from, int to, Color color, int moving_piece) {
+    [[nodiscard]] move_t(int from, int to, Color color, int moving_piece) {
         constexpr int quiet_non_capture = QUIET << 16 | KING << 20;
         m_move = from | to << 6 | color << 12 | moving_piece << 13 | quiet_non_capture;
     }
 
-    Square get_from() const {
+    [[nodiscard]] Square get_from() const {
         return Square(m_move & 0b111111);
     }
 
-    Square get_to() const {
+    [[nodiscard]] Square get_to() const {
         return Square(m_move >> 6 & 0b111111);
     }
 
-    Color get_color() const {
+    [[nodiscard]] Color get_color() const {
         return Color(m_move >> 12 & 0b1);
     }
 
-    PieceType get_piece() const {
+    [[nodiscard]] PieceType get_piece() const {
         return PieceType(m_move >> 13 & 0b111);
     }
 
-    MoveType get_move_type() const {
+    [[nodiscard]] MoveType get_move_type() const {
         return MoveType(m_move >> 16 & 0b1111);
     }
 
-    PieceType get_captured_piece() const {
+    [[nodiscard]] PieceType get_captured_piece() const {
         return PieceType(m_move >> 20 & 0b111);
     }
 
-    void print() const {
-        std::cout << get_from() << " " << get_to() << " " << get_color() << " " << get_piece() <<" " << get_move_type() << " " << get_captured_piece();
-    }
-
-    void print_move() const {
-        std::cout << " " << square_to_string[get_from()] << square_to_string[get_to()] << " ";
-    }
-
-    bool is_promotion() const {
+    [[nodiscard]] bool is_promotion() const {
         return get_move_type() > MoveType::EN_PASSANT;
     }
-    
-    bool is_capture() const {
+
+    [[nodiscard]] bool is_queen_promotion() const {
+        switch (get_move_type()) {
+            case QUIET:
+            case DOUBLE_PAWN_PUSH:
+            case KING_CASTLE:
+            case QUEEN_CASTLE:
+            case CAPTURE:
+            case EN_PASSANT:
+            case KNIGHT_PROMOTION:
+            case BISHOP_PROMOTION:
+            case ROOK_PROMOTION:
+            case KNIGHT_PROMOTION_CAPTURE:
+            case BISHOP_PROMOTION_CAPTURE:
+            case ROOK_PROMOTION_CAPTURE:
+                return false;
+            case QUEEN_PROMOTION_CAPTURE:
+            case QUEEN_PROMOTION:
+                return true;
+        }
+    }
+
+    [[nodiscard]] bool is_capture() const {
         switch (get_move_type()) {
             case QUIET:
             case DOUBLE_PAWN_PUSH:
@@ -94,7 +105,7 @@ public:
         }
     }
 
-    bool is_quiet() const {
+    [[nodiscard]] bool is_quiet() const {
         switch (get_move_type()) {
             case QUIET:
             case DOUBLE_PAWN_PUSH:
@@ -115,7 +126,7 @@ public:
         }
     }
 
-    std::string to_string() const {
+    [[nodiscard]] std::string to_string() const {
         std::string move_string = "";
         move_string.append(square_to_string[get_from()]);
         move_string.append(square_to_string[get_to()]);
@@ -143,17 +154,18 @@ public:
         }
     }
 
-    bool operator==(const move_t & other_move) const {
+    [[nodiscard]] bool operator==(const move_t & other_move) const {
         return (this->m_move & 0x7fffffull) == (other_move.m_move & 0x7fffffull);
         //return m_move == other_move.m_move;
     }
 
-    void set_score(uint32_t score) {
+    void set_score(int score) {
         m_move |= (score << 23);
+        //m_score = score;
     }
 
     uint32_t m_move;
-    //int score;
+    //int m_score;
 };
 
 #endif //MOTOR_MOVE_T_H
