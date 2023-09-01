1ec3964da6a844d7be77221e7eb7e28051695ed3
Martin Novák <martin.novak@gmx.com>
2023-09-01 17:59:55 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,9 @@
+// Created by Martin on 01.09.2023.
+#include <iostream>
+
+int main () {
+    std::cout << "id name Motor " << std::endl;
+    std::cout << "id author Martin Novak" << std::endl;
+    std::cout << "uciok" << std::endl;
+    return 0;
+}
