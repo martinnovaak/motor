01792eee879ce0e61e0975fad56da5615fadbc07
Martin Novák <martin.novak@gmx.com>
2023-09-02 09:33:51 +0200

    Add zobrist hashing

---
+++ zobrist.hpp
@@ -0,0 +1,35 @@
+#ifndef MOTOR_ZOBRIST_HPP
+#define MOTOR_ZOBRIST_HPP
+
+#include "types.hpp"
+#include "zobrist_keys.hpp"
+
+class zobrist {
+public:
+    zobrist() : hash_key(0ull) {}
+
+    void update_castling_hash(std::uint8_t right) {
+        hash_key ^= zobrist_keys::castling_keys[right];
+    }
+
+    void update_side_hash() {
+        hash_key ^= zobrist_keys::side_key;
+    }
+
+    void update_enpassant_hash(Square enpassant_square) {
+        hash_key ^= zobrist_keys::enpassant_keys[enpassant_square];
+    }
+
+    void update_psqt_hash(Color color, Piece piece, Square square) {
+        hash_key ^= zobrist_keys::psqt_keys[color][piece][square];
+    }
+
+    [[nodiscard]] std::uint64_t get_key() const {
+        return hash_key;
+    }
+
+private:
+    std::uint64_t hash_key;
+};
+
+#endif //MOTOR_ZOBRIST_HPP
