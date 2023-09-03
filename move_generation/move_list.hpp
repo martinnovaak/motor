2fef5e46a953a654fd65feeeb78f66279c2d6f5e
Martin Novák <martin.novak@gmx.com>
2023-09-03 16:56:36 +0200

    refactor and fix undo_move method

--- move_generation/move_list.hpp
+++ move_generation/move_list.hpp
@@ -2,14 +2,15 @@
 #define MOTOR_MOVE_LIST_HPP
 
 #include <utility>
-#include <iterator>
 #include "../chess_board/chess_move.hpp"
 
 class move_list {
 private:
-    chess_move list[218];
+    std::array<chess_move, 256> list;
     std::uint8_t count;
 public:
+    using const_iterator = typename std::array<chess_move, 256>::const_iterator;
+
     move_list() : list{}, count{} {}
 
     [[nodiscard]] std::uint8_t size() const {
@@ -25,8 +26,12 @@ public:
         return list[index];
     }
 
-    chess_move * begin() {return std::begin(list);}
-    chess_move * end() {return std::next(std::begin(list), count);}
+    const_iterator begin() const {
+        return list.begin();
+    }
+    const_iterator end() const {
+        return std::next(list.begin(), count);
+    }
 };
 
 #endif //MOTOR_MOVE_LIST_HPP
