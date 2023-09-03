707f7bf8f370b18a616e32252024593e7063818a
Martin Novák <martin.novak@gmx.com>
2023-09-03 17:21:51 +0200

    perft implementation

--- main.cpp
+++ main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 
-#include "chess_board/board.hpp"
+#include "perft.hpp"
 
 int main () {
+    bench();
+
     std::cout << "id name Motor " << std::endl;
     std::cout << "id author Martin Novak" << std::endl;
     std::cout << "uciok" << std::endl;
+
     return 0;
 }
