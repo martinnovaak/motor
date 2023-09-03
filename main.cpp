a9c3c6b016c47d2b63373cc0a4ba7714a577b095
Martin Novák <martin.novak@gmx.com>
2023-09-03 17:18:56 +0200

    implementation of perft

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "perft.hpp"
+
+int main () {
+    bench();
+
+    std::cout << "id name Motor " << std::endl;
+    std::cout << "id author Martin Novak" << std::endl;
+    std::cout << "uciok" << std::endl;
+
+    return 0;
+}
