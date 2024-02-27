7e4b251c6716ad9315c4f912564add451e5f4d08
Martin Novák <novak213@cvut.cz>
2024-02-27 10:05:55 +0100

    tt pruning in qs

--- search/quiescence_search.hpp
+++ search/quiescence_search.hpp
@@ -19,6 +19,21 @@ std::int16_t quiescence_search(board & chessboard, search_data & data, std::int1
 
     std::int16_t eval = evaluate<color>(chessboard);
 
+    Bound flag = Bound::UPPER;
+
+    std::uint64_t zobrist_key = chessboard.get_hash_key();
+    const TT_entry& tt_entry = tt[zobrist_key];
+
+    if (tt_entry.zobrist == zobrist_key) {
+        std::int16_t tt_eval = tt_entry.score;
+        eval = tt_eval;
+        if ((tt_entry.bound == Bound::EXACT) ||
+            (tt_entry.bound == Bound::LOWER && tt_eval >= beta) ||
+            (tt_entry.bound == Bound::UPPER && tt_eval <= alpha)) {
+            return tt_eval;
+        }
+    }
+
     if (eval >= beta) {
         return eval;
     }
@@ -31,6 +46,8 @@ std::int16_t quiescence_search(board & chessboard, search_data & data, std::int1
     generate_all_moves<color, true>(chessboard, movelist);
     qs_score_moves(chessboard, movelist);
 
+    chess_move best_move;
+
     for (std::uint8_t moves_searched = 0; moves_searched < movelist.size(); moves_searched++) {
         chess_move & chessmove = movelist.get_next_move(moves_searched);
 
@@ -42,12 +59,20 @@ std::int16_t quiescence_search(board & chessboard, search_data & data, std::int1
         std::int16_t score = -quiescence_search<enemy_color>(chessboard, data, -beta, -alpha);
         undo_move<color>(chessboard);
 
-        eval = std::max(eval, score);
+        if (score <= eval) {
+            continue;
+        }
+
+        eval = score;
+        best_move = chessmove;
+
         if (eval >= beta) {
+          //  bound = Bound::LOWER;
             break;
         }
         alpha = std::max(eval, alpha);
     }
+
 
     return eval;
 }
