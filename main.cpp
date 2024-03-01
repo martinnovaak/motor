5d95729b40390de89fe5fd4b5126408fbfb8b5ed
Martin Novák <novak213@cvut.cz>
2024-03-01 23:22:43 +0100

    Add bench for openbench

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include "cli/uci.hpp"
 
-int main () {
+int main (int argv, char* argc[]) {
+    if (argv > 1 && std::string{ argc[1] } == "bench") {
+        bench(6);
+    }
     uci_mainloop();
 
     return 0;
