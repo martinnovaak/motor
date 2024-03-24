fb566b954951ef5d65b9318ce04776e93b117cf4
Martin Novák <novak213@cvut.cz>
2024-03-24 21:17:48 +0100

    add improving into nmp

--- search/search.hpp
+++ search/search.hpp
@@ -117,7 +117,7 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
             if (node_type != NodeType::Null && depth >= 3 && eval >= beta && !chessboard.pawn_endgame()) {
                 chessboard.make_null_move<color>();
                 tt.prefetch(chessboard.get_hash_key());
-                int R = 3 + depth / 3;
+                int R = 3 + depth / 3 + improving;
                 data.augment_ply();
                 std::int16_t nullmove_score = -alpha_beta<enemy_color, NodeType::Null>(chessboard, data, -beta, -alpha, depth - R);
                 data.reduce_ply();
