fc73f6fdc153490c51f3f0f084d733c7715cde05
Martin Novák <novak213@cvut.cz>
2024-04-18 13:40:43 +0200

    Fix alphabeta call

--- search/bench.hpp
+++ search/bench.hpp
@@ -63,7 +63,7 @@ std::uint64_t bench_iterative_deepening(board& chessboard, int max_depth) {
     int score;
     for (int depth = 1; depth <= max_depth; depth++) {
         if (depth < 6) {
-            score = alpha_beta<color, NodeType::Root>(chessboard, data, -10'000, 10'000, depth, false);
+            score = alpha_beta<color, NodeType::Root>(chessboard, data, -10'000, 10'000, depth);
         }
         else {
             score = aspiration_window<color>(chessboard, data, score, depth);
