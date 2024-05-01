d3713b4da9a34402548705f5f888597f27870eec
Martin Novák <novak213@cvut.cz>
2024-05-01 18:05:08 +0200

    Simplify lmr table init

--- search/tables/lmr_table.hpp
+++ search/tables/lmr_table.hpp
@@ -4,18 +4,12 @@
 #include <array>
 #include <cmath>
 
-int computeReductionValue(int i, int j) {
-    return static_cast<int>(1.0 + std::log2(static_cast<double>(i)) * std::log2(static_cast<double>(j)) / 5.2);
-}
-
 std::array<std::array<int, 218>, 96> initializeReductions() {
     std::array<std::array<int, 218>, 96> reductions = {};
 
-    reductions[0][0] = 0;
-
-    for (int i = 1; i < 96; ++i) {
-        for (int j = 1; j < 218; ++j) {
-            reductions[i][j] = computeReductionValue(i, j);
+    for (int i = 1; i < 96; i++) {
+        for (int j = 1; j < 218; j++) {
+            reductions[i][j] = static_cast<int>(1.0 + std::log2(static_cast<double>(i)) * std::log2(static_cast<double>(j)) / 5.2);
         }
     }
 
