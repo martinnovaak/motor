548ee227ef89f1a52579eafaf1ba93dcb9a871fc
Martin Novák <martin.novak@gmx.com>
2024-06-30 17:44:07 +0200

    Rename net
    Bench: 4493705

--- evaluation/nnue.hpp
+++ evaluation/nnue.hpp
@@ -30,7 +30,7 @@ struct Weights {
     std::int16_t output_bias;
 };
 
-INCBIN(Weights, "1536-403-81.bin");
+INCBIN(Weights, "nnue.bin");
 const Weights& weights = *reinterpret_cast<const Weights*>(gWeightsData);
 
 enum class Operation {
