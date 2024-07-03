8b1c83de55f6916744c40c3d4fc2641218449f88
Martin Novák <martin.novak@gmx.com>
2024-07-03 08:39:26 +0200

    Cutnode negext
    Bench: 4589707

--- search/search.hpp
+++ search/search.hpp
@@ -228,6 +228,9 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
                 else if (s_beta >= beta) {
                     return s_beta;
                 }
+                else if (cutnode) {
+                    ext = -1;
+                }
             }
         }
 
