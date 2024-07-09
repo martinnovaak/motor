f7ea3b14639b9a7ea2ec4f63c442e4b0f0b2d6b4
Martin Novák <martin.novak@gmx.com>
2024-07-09 14:26:06 +0200

    nmp tweak
    Bench: 4426119

--- search/search.hpp
+++ search/search.hpp
@@ -139,10 +139,10 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
             }
 
             // NULL MOVE PRUNING
-            if (node_type != NodeType::Null && depth >= nmp_depth && eval >= beta && !chessboard.pawn_endgame()) {
+            if (node_type != NodeType::Null && depth >= nmp_depth && eval >= beta && static_eval >= beta && !chessboard.pawn_endgame()) {
                 chessboard.make_null_move<color>();
                 tt.prefetch(chessboard.get_hash_key());
-                int R = nmp + depth / nmp_div + improving;
+                int R = nmp + depth / nmp_div + improving + std::min((static_eval - beta) / 256, 3);
                 data.augment_ply();
                 std::int16_t nullmove_score = -alpha_beta<enemy_color, NodeType::Null>(chessboard, data, -beta, -alpha, depth - R, !cutnode);
                 data.reduce_ply();
