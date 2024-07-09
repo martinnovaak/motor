ef2dc48928fe70af708e8382acee83d839ccb316
Martin Novák <martin.novak@gmx.com>
2024-07-09 13:24:10 +0200

    lmr only quiets
    Bench: 3841846

--- search/search.hpp
+++ search/search.hpp
@@ -253,13 +253,11 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
         else {
             // late move reduction
             bool do_full_search = true;
-            if (depth >= lmr_depth && movelist.get_move_score(moves_searched) < 1'000'000) {
-                if (is_quiet) {
-                    reduction += !is_pv + !improving;
-                    reduction -= chessboard.in_check();
-                    reduction -= movelist.get_move_score(moves_searched) / lmr_quiet_history;
-                    reduction += cutnode * 2;
-                }
+            if (depth >= lmr_depth && is_quiet && movelist.get_move_score(moves_searched) < 1'000'000) {
+                reduction += !is_pv + !improving;
+                reduction -= chessboard.in_check();
+                reduction -= movelist.get_move_score(moves_searched) / lmr_quiet_history;
+                reduction += cutnode * 2;
 
                 reduction = std::clamp(reduction, 0, depth - 2);
 
