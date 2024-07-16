f7200f2759921370d65f472cda70493845232c7e
Martin Novák <martin.novak@gmx.com>
2024-07-16 18:14:30 +0200

    Fix history move ordering
    Bench: 3454953

--- search/tables/history_table.hpp
+++ search/tables/history_table.hpp
@@ -98,10 +98,10 @@ int get_history(board & chessboard, search_data & data, Square from, Square to, 
     if (data.get_ply()) {
         auto prev = data.prev_moves[data.get_ply() - 1];
         move_score += continuation_table[prev.piece_type][prev.to][piece][to];
-        if (data.get_ply()) {
+        if (data.get_ply() > 1) {
             prev = data.prev_moves[data.get_ply() - 2];
             move_score += continuation_table[prev.piece_type][prev.to][piece][to];
-            if (data.get_ply()) {
+            if (data.get_ply() > 3) {
                 prev = data.prev_moves[data.get_ply() - 4];
                 move_score += continuation_table[prev.piece_type][prev.to][piece][to];
             }
