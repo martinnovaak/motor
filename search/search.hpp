9c59b6cfe5bbfcca5c28600a712b58ddc19df48f
Martin Novák <martin.novak@gmx.com>
2024-07-18 07:55:11 +0200

    Bad static eval bonus adjustment
    Bench: 3343220

--- search/search.hpp
+++ search/search.hpp
@@ -298,7 +298,7 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
                     if (is_quiet) {
                         data.update_killer(chessmove);
                     }
-                    update_history<color, is_root>(data, chessboard, best_move, quiets, captures, depth);
+                    update_history<color, is_root>(data, chessboard, best_move, quiets, captures, depth + (static_eval <= alpha));
                     break;
                 }
             }
