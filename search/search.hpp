d5d897871b96f2997bff80f1d666b46f15f5c7d8
Martin Novák <martin.novak@gmx.com>
2024-07-20 01:58:47 +0200

    Simplify capture history and corrhist
    Bench: 3580395

--- search/search.hpp
+++ search/search.hpp
@@ -331,11 +331,10 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
             || (flag == Bound::LOWER && best_score <= static_eval) || (flag == Bound::UPPER && best_score >= static_eval))
         ) {
             int diff = (best_score - raw_eval) * 256;
-            int weight = flag == Bound::EXACT ? std::min(4 * depth * depth + 8 * depth + 4, 512)
-                                              : std::min(3 * depth * depth + 6 * depth + 3, 384);
+            int weight = std::min(depth + 1, 16);
 
             int & entry = correction_table[color][chessboard.get_pawn_key() % 16384];
-            entry = (entry * (4096 - weight) + diff * weight) / 4096;
+            entry = (entry * (256 - weight) + diff * weight) / 256;
             entry = std::clamp(entry, -16'384, 16'384);
         }
 
