8cd65e983772e5c5d4409496677071cf6a0a9ca1
Martin Novák <martin.novak@gmx.com>
2024-07-19 20:35:08 +0200

    Add tt pv
    Bench: 2637372

--- search/search.hpp
+++ search/search.hpp
@@ -264,11 +264,11 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
             // late move reduction
             if (depth >= lmr_depth && movelist.get_move_score(moves_searched) < 1'000'000) {
                 if (is_quiet) {
-                    reduction += !improving;
+                    reduction += !is_pv + !improving;
                     reduction -= chessboard.in_check();
                     reduction -= movelist.get_move_score(moves_searched) / lmr_quiet_history;
-                    reduction += cutnode * 2;
-                    reduction += !tt_pv + !is_pv;
+                    reduction += tt_pv;
+                    reduction += cutnode;
                 }
 
                 reduction = std::clamp(reduction, 0, depth - 2);
