61bbc3c2eef50857014b14008bf1a4cd85eaf4bb
Martin Novák <martin.novak@gmx.com>
2024-07-20 09:35:37 +0200

    negext beta
    Bench: 4122452

--- search/search.hpp
+++ search/search.hpp
@@ -242,7 +242,7 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
                 } else if (cutnode) {
                     ext += -2;
                 } else if (tt_entry.score >= beta && !is_pv) {
-                    ext += -1;
+                    ext += -2;
                 }
             }
         }
