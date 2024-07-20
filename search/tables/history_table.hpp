9038f2579fddaa86be5a676e2414b40cd003c59c
Martin Novák <martin.novak@gmx.com>
2024-07-20 10:06:04 +0200

    See penalty table
    Bench: 3487169

--- search/tables/history_table.hpp
+++ search/tables/history_table.hpp
@@ -11,12 +11,21 @@ std::array<std::array<std::array<std::array<std::array<int, 64>, 64>, 2>, 2>, 2>
 std::array<std::array<std::array<std::array<int, 64>, 6>, 64>, 6> continuation_table = {};
 std::array<std::array<std::array<int, 7>, 64>, 6> capture_table = {};
 std::array<std::array<int, 16384>, 2> correction_table = {};
+std::array<std::array<std::array<int, 7>, 64>, 6> see_penalty_table = {};
 
 constexpr int noisy_mul = 41;
 constexpr int noisy_max = 375;
 constexpr int noisy_gravity = 1779;
 constexpr int quiet_mul = 236;
 constexpr int quiet_max = 2040;
+
+int see_bonus(int depth) {
+    return std::min(8192, 800 * depth);
+}
+
+void update_see_history(int& value, int bonus) {
+    value += bonus - (value * std::abs(bonus) / 49152);
+}
 
 int history_bonus(int depth) {
     return std::min(quiet_max, quiet_mul * depth);
@@ -79,6 +88,7 @@ void update_history(search_data & data, board & chessboard, const chess_move & b
         }
     } else {
         update_cap_history(capture_table[piece][to][chessboard.get_piece(to)], cap_bonus);
+        see_penalty_table[piece][to][chessboard.get_piece(to)] = 0;
     }
 
     for (const auto &capture: captures) {
