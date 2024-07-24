2b5a4c999238147e7246aaf63e0c2808dff301a8
Martin Novák <martin.novak@gmx.com>
2024-07-24 09:39:29 +0200

    Prune in check
    Bench: 3558238

--- search/tables/lmr_table.hpp
+++ search/tables/lmr_table.hpp
@@ -4,12 +4,12 @@
 #include <array>
 #include <cmath>
 
-std::array<std::array<int, 218>, 96> initializeReductions(int lmr = 515) {
+std::array<std::array<int, 218>, 96> initializeReductions(int lmr = 200) {
     std::array<std::array<int, 218>, 96> reductions = {};
 
     for (int i = 1; i < 96; i++) {
         for (int j = 1; j < 218; j++) {
-            reductions[i][j] = static_cast<int>(1.0 + std::log2(static_cast<double>(i)) * std::log2(static_cast<double>(j)) * 100 / lmr);
+            reductions[i][j] = static_cast<int>(0.5 + std::log(static_cast<double>(i)) * std::log(static_cast<double>(j)) * 100 / lmr);
         }
     }
 
