1c6f44fd7cf08dbace668d30ff5933c5a7ef92f5
Martin Novák <martin.novak@gmx.com>
2024-08-09 09:32:40 +0200

    Opponent worsening
    Bench: 2868596

--- search/search.hpp
+++ search/search.hpp
@@ -155,7 +155,7 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
             if (node_type != NodeType::Null && depth >= nmp_depth && eval >= beta && static_eval >= beta && !chessboard.pawn_endgame()) {
                 chessboard.make_null_move<color>();
                 tt.prefetch(chessboard.get_hash_key());
-                int R = nmp + depth / nmp_div + improving + std::min((static_eval - beta) / 256, 3) + opponent_worsening;
+                int R = nmp + depth / nmp_div + improving + std::min((static_eval - beta) / 256, 3) - opponent_worsening;
                 data.augment_ply();
                 std::int16_t nullmove_score = -alpha_beta<enemy_color, NodeType::Null>(chessboard, data, -beta, -alpha, depth - R, !cutnode);
                 data.reduce_ply();
