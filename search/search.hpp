b291832e79f45cbffaf0d5c3bec5900415c21c4f
Martin Novák <martin.novak@gmx.com>
2024-08-12 18:03:36 +0200

    ttpv reduction only quiets
    Bench: 4156662

--- search/search.hpp
+++ search/search.hpp
@@ -304,8 +304,8 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
                     reduction -= chessboard.in_check();
                     reduction -= movelist.get_move_score(moves_searched) / lmr_quiet_history;
                     reduction += cutnode * 2;
+                    reduction -= tt_pv;
                 }
-                reduction -= tt_pv;
 
                 reduction = std::clamp(reduction, 0, depth - 2);
             } else {
