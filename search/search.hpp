82c4f14672f3e573965224bf4191bf50b3c0a12e
Martin Novák <martin.novak@gmx.com>
2024-08-13 23:31:09 +0200

    Keep old iir
    Bench: 4031781

--- search/search.hpp
+++ search/search.hpp
@@ -127,10 +127,9 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
     } else {
         raw_eval = in_check ? -INF : evaluate<color>(chessboard);
         eval = static_eval = correct_eval<color>(chessboard, material_key % 32768, raw_eval);
-    }
-
-    if (tt_move.get_value() == 0 && data.singular_move == 0 && depth >= iir_depth) {
-        depth--;
+        if (data.singular_move == 0 && depth >= iir_depth) {
+            depth--;
+        }
     }
 
     data.improving[data.get_ply()] = static_eval;
