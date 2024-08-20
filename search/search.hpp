a7923ba67a0739e07c109158fc08558e9b521b4c
Martin Novák <martin.novak@gmx.com>
2024-08-20 19:37:09 +0200

    Corrhist tweak
    Bench: 4307373

--- search/search.hpp
+++ search/search.hpp
@@ -370,25 +370,25 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
         if (!(in_check || !chessboard.is_quiet(best_move)
               || (flag == Bound::LOWER && best_score <= static_eval) || (flag == Bound::UPPER && best_score >= static_eval))
         ) {
-            int diff = (best_score - raw_eval) * 256;
-            int weight = std::min(16, depth + 1);
+            int diff = (best_score - raw_eval) * 1024;
+            int weight = std::min(depth * depth + 2 * depth + 1, 128);
 
             int & entry = correction_table[color][chessboard.get_pawn_key() % 16384];
-            entry = (entry * (256 - weight) + diff * weight) / 256;
-            entry = std::clamp(entry, -8'192, 8'192);
+            entry = (entry * (1024 - weight) + diff * weight) / 256;
+            entry = std::clamp(entry, -16'384, 16'384);
 
             int & material_entry = material_correction_table[color][material_key % 32768];
-            material_entry = (material_entry * (256 - weight) + diff * weight) / 256;
-            material_entry = std::clamp(material_entry, -8'192, 8'192);
+            material_entry = (material_entry * (1024 - weight) + diff * weight) / 256;
+            material_entry = std::clamp(material_entry, -16'384, 16'384);
 
             auto [wkey, bkey] = chessboard.get_nonpawn_key();
             int & white_nonpawn_entry = nonpawn_correction_table[color][White][wkey % 16384];
-            white_nonpawn_entry = (white_nonpawn_entry * (256 - weight) + diff * weight) / 256;
-            white_nonpawn_entry = std::clamp(white_nonpawn_entry, -8'192, 8'192);
+            white_nonpawn_entry = (white_nonpawn_entry * (1024 - weight) + diff * weight) / 256;
+            white_nonpawn_entry = std::clamp(white_nonpawn_entry, -16'384, 16'384);
 
             int & black_nonpawn_entry = nonpawn_correction_table[color][Black][bkey % 16384];
-            black_nonpawn_entry = (black_nonpawn_entry * (256 - weight) + diff * weight) / 256;
-            black_nonpawn_entry = std::clamp(black_nonpawn_entry, -8'192, 8'192);
+            black_nonpawn_entry = (black_nonpawn_entry * (1024 - weight) + diff * weight) / 256;
+            black_nonpawn_entry = std::clamp(black_nonpawn_entry, -16'384, 16'384);
         }
 
         if (!would_tt_prune) {
