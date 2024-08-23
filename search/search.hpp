0dd31d042205aea5b3675f708bd03055d5be3bb2
Martin Novák <martin.novak@gmx.com>
2024-08-24 00:25:36 +0200

    Tsoj's nightmare
    Bench: 3998276

--- search/search.hpp
+++ search/search.hpp
@@ -317,7 +317,9 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
 
             if (score > alpha && reduction > 0) {
                 if constexpr (!is_root) {
-                    new_depth += (score > best_score + 80);
+                    if ((score > best_score + 80)) {
+                        new_depth += 1 + (moves_searched > 3 && !is_quiet);
+                    }
                     new_depth -= (score < best_score + new_depth);
                 }
 
