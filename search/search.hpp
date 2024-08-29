d0bc60fb9c66b25d98a3699529f6bdab3dad0538
Martin Novák <martin.novak@gmx.com>
2024-08-29 08:43:16 +0200

    nigtmare extension
    Bench: 4042299

--- search/search.hpp
+++ search/search.hpp
@@ -317,9 +317,7 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
 
             if (score > alpha && reduction > 0) {
                 if constexpr (!is_root) {
-                    if ((score > best_score + 80)) {
-                        new_depth += 1 + (moves_searched > 3);
-                    }
+                    new_depth += ((score > best_score + 80) || (best_score < alpha - 30));
                     new_depth -= (score < best_score + new_depth);
                 }
 
