83e4b74c4d53c23b2fdf3fc436038a328a9f2dcf
Martin Novák <martin.novak@gmx.com>
2024-09-03 07:59:34 +0200

    Remove reduction on nonpv nodes
    Bench: 3980727

--- search/search.hpp
+++ search/search.hpp
@@ -303,7 +303,7 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
             // late move reduction
             if (depth >= lmr_depth && movelist.get_move_score(moves_searched) < 1'000'000) {
                 if (is_quiet) {
-                    reduction += !is_pv + !improving;
+                    reduction += !improving;
                     reduction -= chessboard.in_check();
                     reduction -= movelist.get_move_score(moves_searched) / lmr_quiet_history;
                     reduction += cutnode * 2;
