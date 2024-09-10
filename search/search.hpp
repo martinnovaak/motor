2361b712a4da4ce8c2bf564f174d98917528e495
Martin Novák <martin.novak@gmx.com>
2024-09-10 23:50:16 +0200

    Corrhist weights
    Bench: 3835511

--- search/search.hpp
+++ search/search.hpp
@@ -52,7 +52,7 @@ std::int16_t correct_eval(const board & chessboard, int material_key, int threat
     const int minor_entry = minor_correction_table[color][chessboard.get_minor_key() % 16384];
     auto [wkey, bkey] = chessboard.get_nonpawn_key();
     const int nonpawn_entry = nonpawn_correction_table[color][White][wkey % 16384] + nonpawn_correction_table[color][Black][bkey % 16384];
-    return raw_eval + (entry * 2 + threat_entry + nonpawn_entry + major_entry + minor_entry) / (256 * 3);
+    return raw_eval + (entry * 195 + threat_entry * 102 + nonpawn_entry * 117 + major_entry * 92 + minor_entry * 137) / (256 * 300);
 }
 
 template <Color color, NodeType node_type>
