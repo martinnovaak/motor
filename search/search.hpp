74a67113283ef5ac38c6b5fdc4641a92dea83e93
Martin Novák <martin.novak@gmx.com>
2024-09-14 23:01:14 +0200

    Countermove corrhist
    Bench: 3912971

--- search/search.hpp
+++ search/search.hpp
@@ -89,7 +89,7 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
         tt_move = tt_entry.tt_move;
         std::int16_t tt_eval = tt_entry.score;
         raw_eval = tt_entry.static_eval;
-        eval = static_eval = history->correct_eval<color>(chessboard, raw_eval);
+        eval = static_eval = history->correct_eval<color>(chessboard, data, raw_eval);
         tt_pv = tt_pv || tt_entry.tt_pv;
 
         if constexpr (!is_root) {
@@ -116,7 +116,7 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
         }
     } else {
         raw_eval = in_check ? -INF : evaluate<color>(chessboard);
-        eval = static_eval = history->correct_eval<color>(chessboard, raw_eval);
+        eval = static_eval = history->correct_eval<color>(chessboard, data, raw_eval);
         if (data.singular_move == 0 && depth >= iir_depth) {
             depth--;
         }
@@ -360,7 +360,7 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
         if (!(in_check || !(best_move.get_value() == 0 || chessboard.is_quiet(best_move))
               || (flag == Bound::LOWER && best_score <= static_eval) || (flag == Bound::UPPER && best_score >= static_eval))
         ) {
-            history->update_correction_history<color>(chessboard, best_score, raw_eval, depth);
+            history->update_correction_history<color>(chessboard, data, best_score, raw_eval, depth);
         }
 
         if (!would_tt_prune) {
