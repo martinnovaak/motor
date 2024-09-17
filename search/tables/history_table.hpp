bcfa1020b78c7fe0aa4d7782891ef356acfac487
Martin Novák <martin.novak@gmx.com>
2024-09-17 14:56:16 +0200

    try smaller weight
    Bench: 4592500

--- search/tables/history_table.hpp
+++ search/tables/history_table.hpp
@@ -193,7 +193,7 @@ public:
             cont_entry = continuation_correction_table[prev2.piece_type][prev2.to][prev1.piece_type][prev1.to];
         }
 
-        return raw_eval + (entry * 192 + 115 * material_entry + threat_entry * 88 + nonpawn_entry * 134 + major_entry * 84 + minor_entry * 146 + cont_entry * 150) / (256 * 300);
+        return raw_eval + (entry * 192 + 85 * material_entry + threat_entry * 88 + nonpawn_entry * 134 + major_entry * 84 + minor_entry * 146 + cont_entry * 150) / (256 * 300);
     }
 
 
