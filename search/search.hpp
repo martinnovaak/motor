dc960687f18151ccb0c1c311aeb1fe2fd6eabaca
Martin Novák <martin.novak@gmx.com>
2024-10-11 08:04:54 +0200

    Remove tt reduction
    Bench: 4327352

--- search/search.hpp
+++ search/search.hpp
@@ -81,6 +81,7 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
     chess_move best_move;
     chess_move tt_move = {};
     std::int16_t eval, static_eval, raw_eval;
+    bool would_tt_prune = false;
     bool tt_pv = is_pv;
 
     if (data.singular_move == 0 && tt_entry.zobrist == tt.upper(zobrist_key)) {
@@ -96,9 +97,13 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
                 if ((tt_entry.bound == Bound::EXACT) ||
                     (tt_entry.bound == Bound::LOWER && tt_eval >= beta) ||
                     (tt_entry.bound == Bound::UPPER && tt_eval <= alpha)) {
-                    return tt_eval;
+                    would_tt_prune = true;
                 }
             }
+        }
+
+        if (would_tt_prune && !is_pv) {
+            return tt_eval;
         }
 
         if (!((eval > tt_eval && tt_entry.bound == Bound::LOWER) || (eval < tt_eval && tt_entry.bound == Bound::UPPER)))
@@ -354,7 +359,9 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
             history->update_correction_history<color>(chessboard, data, best_score, raw_eval, depth);
         }
 
-        tt.store(flag, depth, best_score, raw_eval, best_move, data.get_ply(), tt_pv, zobrist_key);
+        if (!would_tt_prune) {
+            tt.store(flag, depth, best_score, raw_eval, best_move, data.get_ply(), tt_pv, zobrist_key);
+        }
     }
 
     return best_score;
