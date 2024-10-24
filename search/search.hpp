766daacc32c2874f2ac5307eca32d703e3d9aea8
Martin Novák <martin.novak@gmx.com>
2024-10-24 08:03:09 +0200

    Reverse futility pruning with corrected evaluation
    Bench: 4554835

--- search/search.hpp
+++ search/search.hpp
@@ -140,7 +140,7 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
 
             // reverse futility pruning
             if (depth < rfp_depth && eval - (rfp - 48 * !is_pv) * (depth - improving) >= beta) {
-                return eval;
+                return history->correct_eval<color>(chessboard, data, eval);
             }
 
             // NULL MOVE PRUNING
