64f2ece9395519fd959fd0079f13e20af7b82187
Martin Novák <martin.novak@gmx.com>
2025-01-05 15:14:45 +0100

    Contcorrhist2 again
    Bench: 4654198

--- search/tables/history_table.hpp
+++ search/tables/history_table.hpp
@@ -23,7 +23,7 @@ public:
     History()
             : history_table({}), material_history_table({}), continuation_table({}), capture_table({}),
               correction_table({}), nonpawn_correction_table({}), minor_correction_table({}),
-              threat_correction_table({}), continuation_correction_table({}) {}
+              threat_correction_table({}), continuation_correction_table({}), continuation_correction_table2({}) {}
 
     void clear() {
         history_table = {};
@@ -35,6 +35,7 @@ public:
         minor_correction_table = {};
         threat_correction_table = {};
         continuation_correction_table = {};
+        continuation_correction_table2 = {};
     }
 
     template <Color color, bool is_root>
@@ -159,6 +160,13 @@ public:
             int &cont_entry = continuation_correction_table[prev2.piece_type][prev2.to][prev1.piece_type][prev1.to];
             cont_entry = (cont_entry * (256 - weight) + diff * weight) / 256;
             cont_entry = std::clamp(cont_entry, -8'192, 8'192);
+
+            if (data.get_ply() > 2) {
+                auto prev3 = data.prev_moves[data.get_ply() - 3];
+                int &cont_entry2 = continuation_correction_table2[prev3.piece_type][prev3.to][prev1.piece_type][prev1.to];
+                cont_entry2 = (cont_entry2 * (256 - weight) + diff * weight) / 256;
+                cont_entry2 = std::clamp(cont_entry2, -8'192, 8'192);
+            }
         }
     }
 
@@ -175,13 +183,18 @@ public:
         const int nonpawn_entry = nonpawn_correction_table[color][White][wkey % 16384] + nonpawn_correction_table[color][Black][bkey % 16384];
 
         int cont_entry = 0;
+        int cont_entry2 = 0;
         if (data.get_ply() > 1) {
             auto prev1 = data.prev_moves[data.get_ply() - 1];
             auto prev2 = data.prev_moves[data.get_ply() - 2];
             cont_entry = continuation_correction_table[prev2.piece_type][prev2.to][prev1.piece_type][prev1.to];
+            if (data.get_ply() > 2) {
+                auto prev3 = data.prev_moves[data.get_ply() - 3];
+                cont_entry2 = continuation_correction_table2[prev3.piece_type][prev3.to][prev1.piece_type][prev1.to];
+            }
         }
 
-        return raw_eval + (entry * 200 + threat_entry * 100 + nonpawn_entry * 200 + minor_entry * 150 + cont_entry * 180) / (256 * 300);
+        return raw_eval + (entry * 200 + threat_entry * 100 + nonpawn_entry * 200 + minor_entry * 150 + cont_entry * 180 + cont_entry2 * 180) / (256 * 300);
     }
 
 
@@ -195,6 +208,7 @@ private:
     std::array<std::array<int, 16384>, 2> minor_correction_table;
     std::array<std::array<int, 32768>, 2> threat_correction_table;
     std::array<std::array<std::array<std::array<int, 64>, 7>, 64>, 7> continuation_correction_table;
+    std::array<std::array<std::array<std::array<int, 64>, 7>, 64>, 7> continuation_correction_table2;
 
     int history_bonus(int depth) const {
         return std::min(2040, 236 * depth);
