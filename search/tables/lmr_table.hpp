e736dd20095cd88467a4df6b42f8c9e52330165a
Martin Novák <martin.novak@gmx.com>
2025-01-10 23:48:24 +0100

    Weird weight formula
    Bench: 4661043

--- search/tables/lmr_table.hpp
+++ search/tables/lmr_table.hpp
@@ -3,6 +3,7 @@
 
 #include <array>
 #include <cmath>
+#include <algorithm>
 
 std::array<std::array<int, 218>, 96> initializeReductions(int lmr = 420) {
     std::array<std::array<int, 218>, 96> reductions = {};
@@ -16,6 +17,21 @@ std::array<std::array<int, 218>, 96> initializeReductions(int lmr = 420) {
     return reductions;
 }
 
+std::array<int, 256> initializeCorrhistBonus() {
+    std::array<int, 256> bonus_table = {};
+
+    for (int j = 1; j <= 256; j++) {
+        double logBase32 = std::log(j + 1) / std::log(32);
+        double sinValue = std::sin(logBase32);
+        double powerValue = std::pow(sinValue, 1.75);
+        double product = powerValue * 256;
+        bonus_table[j - 1] = static_cast<int>(std::min(product, 300.0));
+    }
+
+    return bonus_table;
+}
+
 auto lmr_table = initializeReductions();
+auto corrhist_bonus_table = initializeCorrhistBonus();
 
 #endif //MOTOR_LMR_TABLE_HPP
