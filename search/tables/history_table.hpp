890d693f7b9bc7cb649b7c9af6179e4b90d8c436
Martin Novák <martin.novak@gmx.com>
2025-01-11 19:09:37 +0100

    Remove corrhist
    Bench: 5386225

--- search/tables/history_table.hpp
+++ search/tables/history_table.hpp
@@ -177,7 +177,7 @@ public:
 
     template <Color color>
     std::int16_t correct_eval(const board &chessboard, const search_data &data, int raw_eval) {
-        if (std::abs(raw_eval) > 8'000) return raw_eval;
+        return raw_eval;
         std::uint64_t threat_key = murmur_hash_3(chessboard.get_threats() & chessboard.get_side_occupancy<color>());
 
         const int entry = correction_table[color][chessboard.get_pawn_key() % 16384];
