bb82235054c6b44f6a9ae74b0db450c09b54d212
Martin Novák <martin.novak@gmx.com>
2025-02-05 22:01:52 +0100

    Try reductions/extensions in lmr on noisy moves
    Bench: 4733847

--- search/search.hpp
+++ search/search.hpp
@@ -296,11 +296,13 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
             // late move reduction
             if (depth >= lmr_depth && movelist.get_move_score(moves_searched) < 1'000'000) {
                 if (is_quiet) {
-                    reduction += !is_pv + !improving;
-                    reduction -= chessboard.in_check();
                     reduction -= movelist.get_move_score(moves_searched) / lmr_quiet_history;
                     reduction += cutnode * 2;
+                } else {
+                    reduction += cutnode;
                 }
+                reduction -= chessboard.in_check();
+                reduction += !is_pv + !improving;
                 reduction -= tt_pv;
 
                 reduction = std::clamp(reduction, 0, depth - 2);
