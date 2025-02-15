6242ff5fd72f73e307d8990563289192a5b12b19
Martin Novák <martin.novak@gmx.com>
2025-02-15 17:36:56 +0100

    Disable nmp and probcut in pv nodes
    Bench: 3270784

--- search/search.hpp
+++ search/search.hpp
@@ -17,9 +17,9 @@
 constexpr int iir_depth = 4;
 constexpr int razoring = 500;
 constexpr int razoring_depth = 3;
-constexpr int rfp = 154;
+constexpr int rfp = 100;
 constexpr int rfp_depth = 9;
-constexpr int nmp = 3;
+constexpr int nmp = 4;
 constexpr int nmp_div = 3;
 constexpr int nmp_depth = 3;
 constexpr int lmp_base = 2;
@@ -149,7 +149,7 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
             }
 
             // NULL MOVE PRUNING
-            if (node_type != NodeType::Null && depth >= nmp_depth && eval >= beta && static_eval >= beta && !chessboard.pawn_endgame()) {
+            if (!is_pv && node_type != NodeType::Null && depth >= nmp_depth && eval >= beta && static_eval >= beta && !chessboard.pawn_endgame()) {
                 chessboard.make_null_move<color>();
                 tt.prefetch(chessboard.get_hash_key());
                 int R = nmp + depth / nmp_div + improving + std::min((static_eval - beta) / 245, 3);
@@ -163,7 +163,7 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
             }
 
             const auto probcut_beta = beta + 214;
-            if (depth >= 5 && !(tt_move.get_value() && tt_entry.depth > depth - 3 && tt_entry.score < probcut_beta)) {
+            if (!is_pv && depth >= 5 && !(tt_move.get_value() && tt_entry.depth > depth - 3 && tt_entry.score < probcut_beta)) {
                 const auto see_treshold = probcut_beta - static_eval;
                 move_list movelist;
                 generate_all_moves<color, true>(chessboard, movelist);
