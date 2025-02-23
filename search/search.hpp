7a15d94a17c7dfea970ad434a2b687ed9994e7fb
Martin Novák <martin.novak@gmx.com>
2025-02-23 13:58:12 +0100

    reduce more after singular search in non pv nodes
    Bench: 4303263

--- search/search.hpp
+++ search/search.hpp
@@ -268,7 +268,9 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
                     }
                 } else if (s_beta >= beta) {
                     return s_beta;
-                } else if (tt_entry.score >= beta || cutnode) {
+                } else if (tt_entry.score >= beta) {
+                    ext = -2 - !is_pv;
+                } else if (cutnode) {
                     ext = -2;
                 }
             }
