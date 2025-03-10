a5e36b247ddcf8faf9fabcc16d364998306c1f51
Martin Novák <martin.novak@gmx.com>
2025-03-10 20:52:33 +0100

    Pawn threat history
    Bench: 4850888

--- search/tables/history_table.hpp
+++ search/tables/history_table.hpp
@@ -54,7 +54,7 @@ public:
             bool threat_from = (threats & bb(from));
             bool threat_to = (threats & bb(to));
             update_history(history_table[color][threat_from][threat_to][from][to], bonus);
-            update_history(pawn_history_table[color][pawn_key][piece][to], bonus);
+            update_history(pawn_history_table[color][threat_from][threat_to][pawn_key][piece][to], bonus);
 
             if constexpr (!is_root) {
                 prev = data.prev_moves[data.get_ply() - 1];
@@ -76,7 +76,7 @@ public:
                 bool qthreat_from = (threats & bb(qfrom));
                 bool qthreat_to = (threats & bb(qto));
                 update_history(history_table[color][qthreat_from][qthreat_to][qfrom][qto], penalty);
-                update_history(pawn_history_table[color][pawn_key][qpiece][qto], penalty);
+                update_history(pawn_history_table[color][threat_from][threat_to][pawn_key][qpiece][qto], penalty);
 
                 if constexpr (!is_root) {
                     update_history(continuation_table[color][prev.piece_type][prev.to][qpiece][qto], penalty);
@@ -106,7 +106,7 @@ public:
         const std::uint64_t pawn_key = chessboard.get_pawn_key() % 512;
 
         int move_score = history_table[color][threat_from][threat_to][from][to];
-        move_score += pawn_history_table[color][pawn_key][piece][to];
+        move_score += pawn_history_table[color][threat_from][threat_to][pawn_key][piece][to];
 
         int ply = data.get_ply();
         if (ply > 0) {
@@ -199,7 +199,7 @@ public:
 
 private:
     std::array<std::array<std::array<std::array<std::array<int, 64>, 64>, 2>, 2>, 2> history_table;
-    std::array<std::array<std::array<std::array<int, 64>, 7>, 512>, 2> pawn_history_table;
+    std::array<std::array<std::array<std::array<std::array<std::array<int, 64>, 7>, 512>, 2>, 2>, 2> pawn_history_table;
     std::array<std::array<std::array<std::array<std::array<int, 64>, 7>, 64>, 7>, 2> continuation_table;
     std::array<std::array<std::array<int, 7>, 64>, 6> capture_table;
     std::array<std::array<int, 16384>, 2> pawn_correction_table;
