9852a1a40df847b04e1901bed675dd77d1ddc15d
Martin Novák <martin.novak@gmx.com>
2025-03-31 20:24:14 +0200

    Different material scale
    Bench: 5059485

--- search/tables/history_table.hpp
+++ search/tables/history_table.hpp
@@ -193,7 +193,7 @@ public:
             }
         }
 
-        return raw_eval + (pawn_entry * 200 + threat_entry * 100 + nonpawn_entry * 200 + minor_entry * 150 + major_entry * 120 + cont_entry * 180 + cont_entry2 * 180) / (256 * 300);
+        return raw_eval + (pawn_entry * 210 + threat_entry * 100 + nonpawn_entry * 225 + minor_entry * 155 + major_entry * 125 + cont_entry * 180 + cont_entry2 * 180) / (256 * 300);
     }
 
 
