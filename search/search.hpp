234996dbe07808d02067acb4458b57ef358bb633
Martin Novák <martin.novak@gmx.com>
2025-04-17 07:05:22 +0200

    Allow lmr on good captures
    Bench: 4899788

--- search/search.hpp
+++ search/search.hpp
@@ -289,10 +289,13 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
             score = -alpha_beta<enemy_color, NodeType::PV>(chessboard, data, -beta, -alpha, new_depth, false);
         } else {
             // late move reduction
-            if (depth >= lmr_depth && movelist.get_move_score(moves_searched) < 1'000'000) {
+            if (depth >= lmr_depth) {
                 if (is_quiet) {
                     reduction -= movelist.get_move_score(moves_searched) / lmr_quiet_history;
+                } else if (movelist.get_move_score(moves_searched) < 1'000'000) {
+                    reduction -= 2;
                 }
+
                 reduction += !improving;
                 reduction -= tt_pv;
                 reduction += cutnode * 2;
