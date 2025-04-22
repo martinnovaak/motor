a8018339ce58f162780baf9124a7ea2f0f1aea24
Martin Novák <martin.novak@gmx.com>
2025-04-22 07:01:31 +0200

    Square correction history
    Bench: 4423346

--- search/tables/history_table.hpp
+++ search/tables/history_table.hpp
@@ -205,7 +205,7 @@ public:
             }
         }
 
-        return raw_eval + (pawn_entry * 200 + threat_entry * 100 + nonpawn_entry * 200 + minor_entry * 150 + major_entry * 120 + cont_entry * 180 + cont_entry2 * 180 + square_entry * 100) / (256 * 300);
+        return raw_eval + (pawn_entry * 200 + threat_entry * 100 + nonpawn_entry * 200 + minor_entry * 150 + major_entry * 120 + cont_entry * 180 + cont_entry2 * 180 + square_entry * 70) / (256 * 300);
     }
 
 
