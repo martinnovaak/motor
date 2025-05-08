38ebcc71a4c9d8bb535620b64f172f8907c3dd5d
Martin Novák <martin.novak@gmx.com>
2025-05-08 07:04:46 +0200

    lmr history clamp
    Bench: 4404886

--- search/search.hpp
+++ search/search.hpp
@@ -291,7 +291,7 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
             // late move reduction
             if (depth >= lmr_depth && movelist.get_move_score(moves_searched) < 1'000'000) {
                 if (is_quiet) {
-                    reduction -= movelist.get_move_score(moves_searched) / lmr_quiet_history;
+                    reduction -= std::clamp(static_cast<int>(movelist.get_move_score(moves_searched) / lmr_quiet_history), -2, 2);
                 }
                 reduction += !improving;
                 reduction -= tt_pv;
