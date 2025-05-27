4fe182bffa64e702891e0ec9dbae8786b6149da8
Martin Novák <martin.novak@gmx.com>
2025-05-27 06:46:08 +0200

    static eval in lmp
    Bench: 5467432

--- search/search.hpp
+++ search/search.hpp
@@ -229,7 +229,7 @@ std::int16_t alpha_beta(board& chessboard, search_data& data, std::int16_t alpha
         if constexpr (!is_root) {
             if (moves_searched && best_score > -9'000 && !in_check && movelist[moves_searched] < 20'000) {
                 if (is_quiet) {
-                    if (quiets.size() > lmp_base + depth * depth / (2 - improving)) {
+                    if (quiets.size() > lmp_base + depth * depth / (2 - (improving || static_eval >= beta))) {
                         break;
                     }
 
