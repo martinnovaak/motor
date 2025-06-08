ff20e8df6ae5ad429b0a4321e46aec9d15285a2c
Martin Novák <martin.novak@gmx.com>
2025-06-08 19:42:25 +0200

    Different square structure
    Bench: 5382808

--- search/tables/history_table.hpp
+++ search/tables/history_table.hpp
@@ -200,7 +200,7 @@ public:
             }
         }
 
-        return raw_eval + (pawn_entry * 200 + threat_entry * 100 + square_entry * 100 + nonpawn_entry * 200 + minor_entry * 150 + major_entry * 120 + cont_entry * 180 + cont_entry2 * 180) / (256 * 300);
+        return raw_eval + (pawn_entry * 200 + threat_entry * 100 + square_entry * 50 + nonpawn_entry * 200 + minor_entry * 150 + major_entry * 120 + cont_entry * 180 + cont_entry2 * 180) / (256 * 300);
     }
 
 
